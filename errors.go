4c529eece6576afb08de1ae3816d1a90784ed884
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-02-01 00:03:35 +0100

    outputting errors properly

---
+++ errors.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrValidation = errors.New("validation failed")
+)
+
+type stepErr struct {
+	step string
+	msg string 
+	cause error 
+}
+
+func (s *stepErr) Error() string {
+	return fmt.Sprintf("Step: %q: %s: Cause: %w", s.step, s.msg, s.cause)
+}
+
+func (s *stepErr) Is(target error) bool {
+	t, ok := target.(*stepErr)
+
+	if !ok {
+		return false 
+	}
+
+	return t.step == s.step
+}
+
+func (s *stepErr) Unwrap() error {
+	return s.cause
+}
