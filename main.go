ccde9d8929607875cc82ea0ec63f28bb95319094
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-02-01 00:06:58 +0100

    done with alpha

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func run(proj, commitMsg string, out io.Writer) error {
+	if proj == "" {
+		return fmt.Errorf("project directory is required: %w", ErrValidation)
+	}
+
+	if commitMsg == "" {
+		return fmt.Errorf("commit message is required: %w", ErrValidation)
+	}
+
+	pipeline := make([]step, 4)
+
+	pipeline[0] = newStep(
+		"git add",
+		"git",
+		"Git add: SUCCESS",
+		proj,
+		[]string{"add", "."},
+	)
+
+	pipeline[1] = newStep(
+		"git commit",
+		"git",
+		"Git commit: SUCCESS",
+		proj,
+		[]string{"commit", "-m", commitMsg},
+	)
+
+	pipeline[2] = newStep(
+		"Generating static files",
+		"npm",
+		"Generating static files: SUCCESS",
+		proj,
+		[]string{"run", "generate"},
+	)
+
+	pipeline[3] = newStep(
+		"Pushing to the Dev repo",
+		"git",
+		"Git push : SUCCESS",
+		proj,
+		[]string{"push", "-u", "origin", "master"},
+	)
+
+	for _, s := range pipeline {
+		msg, err := s.execute()
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintln(out, msg)
+		if err != nil {
+			return err 
+		}
+	}
+	
+	return nil
+}
+
+func main() {
+	proj := flag.String("p", "", "Project directory")
+	msg := flag.String("m", "", "Commit message")
+	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	if err := run(*proj, *msg, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
