333b592d7365192eaae89fd80a2f34b3f738935f
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-03-17 16:30:32 +0100

    ...

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
-	"os"
-	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func run(proj, commitMsg string, out io.Writer) error {
@@ -18,41 +22,33 @@ func run(proj, commitMsg string, out io.Writer) error {
 		return fmt.Errorf("commit message is required: %w", ErrValidation)
 	}
 
-	pipeline := make([]step, 4)
+	f, err := os.Open(filepath.Join(proj, ".aang"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-	pipeline[0] = newStep(
-		"git add",
-		"git",
-		"Git add: SUCCESS",
-		proj,
-		[]string{"add", "."},
-	)
+	scanner := bufio.NewScanner(f)
+	var pipeline []step
+	for scanner.Scan() {
+		command := scanner.Text()
+		cmdArray := strings.Split(command, " ")
+		exe := cmdArray[0]
+		pipeline = append(pipeline, newStep(
+			command,
+			exe,
+			command[0:2] + ": SUCCESS",
+			proj,
+			cmdArray[1:],
+		),
+		)
+	}
 
-	pipeline[1] = newStep(
-		"git commit",
-		"git",
-		"Git commit: SUCCESS",
-		proj,
-		[]string{"commit", "-m", commitMsg},
-	)
-
-	pipeline[2] = newStep(
-		"Generating static files",
-		"npm",
-		"Generating static files: SUCCESS",
-		proj,
-		[]string{"run", "generate"},
-	)
-
-	pipeline[3] = newStep(
-		"Pushing to the Dev repo",
-		"git",
-		"Git push : SUCCESS",
-		proj,
-		[]string{"push", "-u", "origin", "master"},
-	)
-
-	for _, s := range pipeline {
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	
+	for _, s := range pipeline {		
 		msg, err := s.execute()
 		if err != nil {
 			return err
@@ -68,14 +64,17 @@ func run(proj, commitMsg string, out io.Writer) error {
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+		"aang-cli v0.0.1-alpha \n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright " + strconv.Itoa(time.Now().Local().Year()) + "\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
 	proj := flag.String("p", "", "Project directory")
 	msg := flag.String("m", "", "Commit message")
 	flag.Parse()
 
-	err := godotenv.Load()
-	if err != nil {
-	  log.Fatal("Error loading .env file")
-	}
 
 	if err := run(*proj, *msg, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
--- step.go
+++ step.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"log"
-	"os"
 	"os/exec"
-	"strconv"
-	"strings"
-	"bufio"
 )
 
 type step struct {
@@ -33,18 +26,6 @@ func (s step) execute() (string, error) {
 	cmd := exec.Command(s.exe, s.args...)
 	cmd.Dir = s.proj
 
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	stdout, err := cmd.StdoutPipe()
-
-	if err != nil {
-		return "", &stepErr{
-			step: s.name,
-			msg: "failed to execute",
-			cause: err,
-		}
-	}
-
 	if err := cmd.Run(); err != nil {
 		return "", &stepErr{
 			step: s.name,
@@ -52,45 +33,43 @@ func (s step) execute() (string, error) {
 			cause: err,
 		}
 	}
-
-	buf := bufio.NewReader(stdout)
 	
-	if s.name == "git commit" {
-		output := strings.Split(out.String(), "")
-		commitSha := strings.Join(output[8:15], "")
-		env := strings.Split(os.Getenv("APP_VERSION"), "")
-		versionNum, _ := strconv.Atoi(strings.Join(env[4:6], "")) 
+	// if s.name == "git commit" {
+	// 	output := strings.Split(out.String(), "")
+	// 	commitSha := strings.Join(output[8:15], "")
+	// 	env := strings.Split(os.Getenv("APP_VERSION"), "")
+	// 	versionNum, _ := strconv.Atoi(strings.Join(env[4:6], "")) 
 
-		versionNum++
-		newVersionNum := strconv.Itoa(versionNum)
-		file, err := os.OpenFile(".env", os.O_RDWR, 0644)
+	// 	versionNum++
+	// 	newVersionNum := strconv.Itoa(versionNum)
+	// 	file, err := os.OpenFile(".env", os.O_RDWR, 0644)
 
-		if err != nil {
-			log.Fatalf("failed opening file: %s", err)
-		}
-		defer file.Close()
+	// 	if err != nil {
+	// 		log.Fatalf("failed opening file: %s", err)
+	// 	}
+	// 	defer file.Close()
 
-		// edit this depending on your .env
-		_, err = file.WriteAt([]byte(newVersionNum), 18)
+	// 	// edit this depending on your .env
+	// 	_, err = file.WriteAt([]byte(newVersionNum), 18)
 
-		if err != nil {
-			log.Fatalf("failed writing to file: %s", err)
-		}
+	// 	if err != nil {
+	// 		log.Fatalf("failed writing to file: %s", err)
+	// 	}
 
-		_, err = file.WriteAt([]byte(commitSha), 28)
+	// 	_, err = file.WriteAt([]byte(commitSha), 28)
 
-		if err != nil {
-			log.Fatalf("failed writing to file: %s", err)
-		}
-	}
+	// 	if err != nil {
+	// 		log.Fatalf("failed writing to file: %s", err)
+	// 	}
+	// }
 
-	if s.name == "Generating static files" {
-		for {
-			output, _, _ := buf.ReadLine()
-			fmt.Fprintln(os.Stdout, string(output))
-		}
+	// if s.name == "Generating static files" {
+	// 	for {
+	// 		output, _, _ := buf.ReadLine()
+	// 		fmt.Fprintln(os.Stdout, string(output))
+	// 	}
 		
-	}
+	// }
 
 	return s.message, nil
 }
